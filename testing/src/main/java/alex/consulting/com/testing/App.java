216786544c2e1a09b10842c30964669bc82ff4f2
JoshKingswell <josh.kingswell@gmail.com>
2017-11-27 12:06:53 +0000

    First Commit

---
+++ testing/src/main/java/alex/consulting/com/testing/App.java
@@ -0,0 +1,13 @@
+package alex.consulting.com.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
